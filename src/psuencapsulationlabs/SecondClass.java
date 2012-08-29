14a0c03c8421b480a8952b9544682fbea5975d36
purban1 <purban1@L11712038611.wctc.msft>
2012-08-29 18:15:21 -0500

    Changed my mind and reverted again.

---
+++ src/psuencapsulationlabs/SecondClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psuencapsulationlabs;
+
+/**
+ *
+ * @author purban1
+ */
+public class SecondClass {
+    private int y;
+}
