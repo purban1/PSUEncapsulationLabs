f7543e58802832095c681baf38837b7d62d8d228
purban1 <purban1@L11712038611.wctc.msft>
2012-08-29 18:13:44 -0500

    Reverting to original demo

--- src/psuencapsulationlabs/PSUEncapsulationLabs.java
+++ src/psuencapsulationlabs/PSUEncapsulationLabs.java
@@ -15,6 +15,5 @@ public class PSUEncapsulationLabs {
      */
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Good bye!!");
     }
 }
